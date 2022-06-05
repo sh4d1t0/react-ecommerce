cc961ac29a94e75420a73e9c903f711e3d89e240
sh4d1t0 <shadito@live.com.mx>
2022-06-05 17:17:27 -0500

    add Redux and example

---
+++ src/Reduxample.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Counter } from './features/counter/Counter'
+
+function Reduxample() {
+  return (
+    <>
+      <Counter />
+    </>
+  )
+}
+
+export default Reduxample
