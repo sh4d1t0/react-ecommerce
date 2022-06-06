ab22777e70d4b7d62c7298ca01dfe0960dd6fa12
sh4d1t0 <shadito@live.com.mx>
2022-06-06 01:20:50 -0500

    responsive design for products

--- src/components/molecules/ProductCard/index.tsx
+++ src/components/molecules/ProductCard/index.tsx
@@ -14,60 +14,63 @@ export const ProductCard = () => {
   return (
     <>
       {allProducts.map(product => (
-        <div key={product.id}>
-          <div className="py-6">
-            <div className="flex max-w-md overflow-hidden rounded-lg bg-white shadow-lg">
-              <div
-                className="w-1/3 bg-cover"
-                style={{
-                  backgroundImage: `url(${product.image})`,
-                }}
-              ></div>
-              <div className="w-2/3 p-4">
-                <h1 className="text-2xl font-bold text-gray-900">
-                  {product.title}
-                </h1>
-                <p className="mt-2 text-sm text-gray-600">
-                  {product.description}
-                </p>
-                <div className="item-center mt-2 flex">
-                  <svg
-                    className="h-5 w-5 fill-current text-gray-700"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-                  </svg>
-                  <svg
-                    className="h-5 w-5 fill-current text-gray-700"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-                  </svg>
-                  <svg
-                    className="h-5 w-5 fill-current text-gray-700"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-                  </svg>
-                  <svg
-                    className="h-5 w-5 fill-current text-gray-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-                  </svg>
-                  <svg
-                    className="h-5 w-5 fill-current text-gray-500"
-                    viewBox="0 0 24 24"
-                  >
-                    <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
-                  </svg>
+        <div className="py-6 px-4" key={product.id}>
+          <div className="flex max-w-md overflow-hidden rounded-lg bg-white shadow-lg">
+            <div className="mx-auto max-w-md overflow-hidden rounded-xl bg-white shadow-md md:max-w-2xl">
+              <div className="md:flex">
+                <div className="md:shrink-0">
+                  <img
+                    className="h-48 w-full object-cover md:h-full md:w-40"
+                    src={product.image}
+                    alt={product.title}
+                  />
                 </div>
-                <div className="item-center mt-3 flex justify-between">
-                  <h1 className="text-xl font-bold text-gray-700">
-                    ${product.price}
+                <div className="p-4">
+                  <h1 className="text-2xl font-semibold uppercase tracking-wide text-indigo-500">
+                    {product.title}
                   </h1>
-                  <div className="px-3 py-2">
-                    <Button variant="contained">Agregar al carrito</Button>
+                  <p className="mt-2 text-sm text-slate-500">
+                    {product.description}
+                  </p>
+                  <div className="item-center mt-2 flex">
+                    <svg
+                      className="h-5 w-5 fill-current text-gray-700"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+                    </svg>
+                    <svg
+                      className="h-5 w-5 fill-current text-gray-700"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+                    </svg>
+                    <svg
+                      className="h-5 w-5 fill-current text-gray-700"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+                    </svg>
+                    <svg
+                      className="h-5 w-5 fill-current text-gray-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+                    </svg>
+                    <svg
+                      className="h-5 w-5 fill-current text-gray-500"
+                      viewBox="0 0 24 24"
+                    >
+                      <path d="M12 17.27L18.18 21L16.54 13.97L22 9.24L14.81 8.63L12 2L9.19 8.63L2 9.24L7.46 13.97L5.82 21L12 17.27Z" />
+                    </svg>
+                  </div>
+                  <div className="item-center mt-3 flex justify-between">
+                    <h1 className="text-sm font-bold text-gray-700">
+                      ${product.price}
+                    </h1>
+                    <div className="px-3 py-2">
+                      <Button variant="contained">Agregar al carrito</Button>
+                    </div>
                   </div>
                 </div>
               </div>
--- src/components/organism/ProductsList/index.tsx
+++ src/components/organism/ProductsList/index.tsx
@@ -5,12 +5,7 @@ export const ProductsList = () => {
   return (
     <>
       <div className="flex min-h-full flex-wrap justify-center px-4">
-        <div className="grid grid-cols-3 gap-2">
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-          <ProductCard />
-        </div>
+        <ProductCard />
       </div>
     </>
   )
